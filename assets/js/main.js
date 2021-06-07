6c3217f607280873d68eb9ca212b49bf1d06bb13
matijaskorup1991 <smatija1991@gmail.com>
2021-06-07 11:30:19 +0200

    end

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+let $ = (el) => document.querySelector(el);
+let $$ = (el) => Array.from(document.querySelectorAll(el));
+let $on = (el, evt, fn) =>
+  Array.isArray(el)
+    ? el.forEach((o) => $on(o, evt, fn))
+    : el.addEventListener(evt, fn);
+
+for (let i = 1; i <= 100; i++) {
+  $('#snakeField').innerHTML += '<div class="item"></div>';
+}
+
+function random() {
+  let { floor, random } = Math;
+  return `rgb(${floor(random() * 255 + 1)},${floor(random() * 255 + 1)},${floor(
+    random() * 255 + 1
+  )})`;
+}
+
+$on($$('div'), 'mouseover', (e) => {
+  e.target.style.backgroundColor = random();
+});
+
+$on($$('div'), 'mouseover', (e) => {
+  setTimeout(() => {
+    e.target.style.backgroundColor = 'lightblue';
+  }, 500);
+});
